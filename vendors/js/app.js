ac2d5e0049a4c667c07744cdd13833a66aea2980
aurora <selva_beat@hotmail.com>
2017-12-19 18:34:53 -0500

    agregando ARCHIVO CON JQUERY

---
+++ vendors/js/app.js
@@ -0,0 +1,15 @@
+$('document').ready(function() {
+  console.log($('#paragraph').text());
+  console.log($('#paragraph').html());
+  console.log($('.input').val());
+  console.log(jQuery('.input').val());
+  $('p').text('hola');
+  $('p:first-child').html('tilde');
+});
+
+
+
+
+
+
+
